7b4025c52e210fcb657a703d65f887b12fbe022b
jeffreyfaatz <jeff_faatz@rlesystems.online>
2024-01-25 09:54:46 -0500

    HOST comment

--- PyChat.py
+++ PyChat.py
@@ -5,7 +5,7 @@ import random
 
 # TCP connection
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-HOST = "127.0.0.1" # change to public IP of server if not being used on a local network
+HOST = "127.0.0.1" # change to public IP of server if not being used on a local
 PORT = 5005
 client.connect((HOST, PORT))
 
